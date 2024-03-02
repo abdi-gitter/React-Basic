dae6c24d1e817dd27007dbb766e8339464c27c58
Abdisa Bedada <abdisat.bedada@gmail.com>
2024-03-01 23:43:39 -0600

    dish

---
+++ Dish.js
@@ -0,0 +1,12 @@
+const dish = React.createElement("section", {id: "baked-salmon"},
+        React.createElement("h1", null, "Baked Salmon"),
+        React.createElement("ul", {"className": "ingredients"},
+            React.createElement("li", null, "1 lb Salmon"),
+            React.createElement("li", null, "1 cup Pine Nuts"),
+            React.createElement("li", null, "2 cups Butter Lettuce"),
+            React.createElement("li", null, "1 Yellow Squash"),
+            React.createElement("li", null, "1/2 cup Olive Oil"),
+            React.createElement("li", null, "3 cloves of Garlic")
+        )
+);
+console.log(dish)
