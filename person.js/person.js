461d6d7d6e07d9eba462cc563d5c37a3c1a9d2d1
Abdisa Bedada <abdisat.bedada@gmail.com>
2024-03-03 19:02:54 -0600

    function as constructor

---
+++ person.js/person.js
@@ -0,0 +1,8 @@
+function Person(firstName, lastName){
+    this.firstName = firstName
+    this.lastName = lastName
+    }
+    const person = new Person('Abdisa', 'Bedada')
+    person.nationality = 'Ethiopian'
+    person.age = 44
+    console.log(Person)
