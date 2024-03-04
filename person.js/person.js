3c214f73b7f2bdb74db5e381d162c7cab97f9c80
Abdisa Bedada <abdisat.bedada@gmail.com>
2024-03-03 19:12:58 -0600

    changed folder

--- person.js/person.js
+++ person.js/person.js
@@ -4,5 +4,9 @@ function Person(firstName, lastName){
     }
     const person = new Person('Abdisa', 'Bedada')
     person.nationality = 'Ethiopian'
+    person.prototype.nationality = 'American'
+    const person1 = new Person('Menen' 'Bedada')
+    console.log(person1)
+  
     person.age = 44
     console.log(Person)
